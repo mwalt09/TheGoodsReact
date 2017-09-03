cabfec908a1ade35ad242967f17e305118404acc
bgorwin <brent.gorwin@gmail.com>
2017-09-03 18:13:26 -0500

    Got the box, img, and description on Search.js

--- app/components/children/Search.js
+++ app/components/children/Search.js
@@ -4,23 +4,41 @@ var Link = require("react-router").Link;
 
 // Code from React-Bootstrap for Col.
 
-import { Container, Row, Col } from 'reactstrap';
-
+import { Container, Row, Col, Well, Media  } from 'react-bootstrap';
 
 var Search = React.createClass({
 
   	// Here we render the component
   	render: function() {
-
+      var itemCardStyle = {
+        width: 400,
+        height: 400,
+        border: 1,
+        borderColor: 'black',
+        borderStyle: 'solid', 
+      };
    		return (
-        <container>
-          <div id="rowForItems ">
-            <Row>
-              <Col xs="6"></Col>
-            </Row>
-          </div>
 
-        <div>
+
+
+        <div className="cardsRenderContainer">
+
+
+      <Media style={itemCardStyle}>
+       <Media.Left>
+          <img width={64} height={64} src="/assets/images/thumbnail.png" alt="Image"/>
+        </Media.Left>
+        <Media.Body>
+          <Media.Heading>Media Heading</Media.Heading>
+          <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+        </Media.Body>
+      </Media>
+
+
+
+
+
+
        			<div>
        				<p>
                	<Link to="/Search/Results"><button className="btn btn-primary btn-lg">Results</button></Link>
@@ -30,9 +48,9 @@ var Search = React.createClass({
        			<div>
     				{this.props.children}
     			  </div>
-        </div>
-      </container>
-		);
+    </div>
+
+		  );
   	}
 });
 
