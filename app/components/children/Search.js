429c85054b303880332d322b20b7e5cbc4abb484
Joseph Kohatsu <jpkohatsu@gmail.com>
2017-09-04 19:15:54 -0500

    9/4 full lifecycle event for searchItem

--- app/components/children/Search.js
+++ app/components/children/Search.js
@@ -1,26 +1,68 @@
 var React = require("react");
+var Results = require("./grandchildren/Results");
+var SearchBar = require("./grandchildren/SearchBar");
+var helpers = require("../utils/helpers");
 // Including the Link component from React Router to navigate within our application without full page reloads
 var Link = require("react-router").Link;
 
+// Creating the Search Form component
 var Search = React.createClass({
 
-  	// Here we render the component
-  	render: function() {
+  componentDidUpdate: function() {
+    helpers.getSearch(this.state.searchTerm).then(function(response) {
+      console.log("response: " + JSON.stringify(response));
+      console.log("response data itemName: " + response.data[0].itemName);
+      // this.setState({itemName: ""});
+      console.log("this is the stat itemName: " + this.state.itemName);
 
-   		return (
-        <div>
-     			<div>
-     				<p>
-             	<Link to="/Search/Results"><button className="btn btn-primary btn-lg">Results</button></Link>
-              <Link to="/Search/Details"><button className="btn btn-danger btn-lg">Details</button></Link>
+
+      if (response.data[0].itemName !== this.state.itemName) {
+        console.log("we got to this point");
+        this.setState({ itemName : response.data[0].itemName });
+      }
+    }.bind(this));
+  },
+
+  // Here we set a generic state associated with the text being searched for
+  getInitialState: function() {
+    return { term: "", itemName: ""
+  };
+  },
+
+  // This function allows childrens to update the parent.
+  setTerm: function(term) {
+    this.setState({ searchTerm: term });
+  },
+  
+  // Here we render the component
+  render: function() {
+   	return (
+        <div className="container">
+        <div className="row">
+          <div className="jumbotron">
+            <h2 className="text-center">Address Finder!</h2>
+            <p className="text-center">
+              <em>Enter a landmark to search for its exact address (ex: "Eiffel Tower").</em>
             </p>
-     			</div>
-     			<div>
-  				{this.props.children}
-  			  </div>
+          </div>
+
+          <div className="col-md-6">
+
+            <SearchBar setTerm={this.setTerm} />
+
+          </div>
+
+          <div className="col-md-6">
+
+            <Results itemName={this.state.itemName} />
+
+          </div>
+
+        </div>
+
       </div>
-		);
-  	}
+	);
+}
 });
 
 // Export the component back for use in other files
